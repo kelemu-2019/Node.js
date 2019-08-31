879ad29f7de6cec44685cb2a017e72cd1eb03a1e
Kelemu <50927723+kelemu-2019@users.noreply.github.com>
2019-08-31 14:26:15 +0200

    Add files via upload

--- week1/homework/src/subtractstate.js
+++ week1/homework/src/subtractstate.js
@@ -1,5 +1,5 @@
 function subtractState(state, response) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.writeHead(200, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify('state:' + (state - 1)));
 }
 
