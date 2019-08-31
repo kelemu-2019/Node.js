4e7c97c59079d5148217ffe7eaeb5ca3ba6518cc
Kelemu <50927723+kelemu-2019@users.noreply.github.com>
2019-08-31 14:09:06 +0200

    Add files via upload

--- week1/homework/src/resetstate.js
+++ week1/homework/src/resetstate.js
@@ -1,17 +1,6 @@
 function resetState(state, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(`
-    <!html>
-    <html>
-      <head>
-        <title>resetState</title>
-        <link href="styles.css" type="text/css" rel="stylesheet"/>
-      </head>
-      <body>
-      <h2>state: ${state}</h2>
-      </body>
-    </html>
-  `);
+  response.write(JSON.stringify('state: ' + state));
 }
 
 module.exports = resetState;
