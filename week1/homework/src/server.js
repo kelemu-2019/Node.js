eed66d3db14100e6751366d29ec5fdc98bf5b2c6
Kelemu <50927723+kelemu-2019@users.noreply.github.com>
2019-09-01 12:12:32 +0200

    Add files via upload

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,51 +1,38 @@
 'use strict';
 
 const http = require('http');
-const initialState = require('./initialState');
-const addState = require('./addstate');
-const subtractState = require('./subtractstate');
-const resetState = require('./resetstate');
-const errorState = require('./statuserrorcode');
+
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-
-function showState(state, request, response) {
-  console.log('hi im in the function');
-  let url = request.url;
-
-  switch (url) {
-    case '/state':
-      initialState(state, response);
-      break;
-
-    case '/add':
-      addState(state, response);
-      break;
-
-    case '/subtract':
-      subtractState(state, response);
-      break;
-
-    case '/reset':
-      resetState(state, response);
-      break;
-
-    default:
-      errorState(state, response);
-
-      break;
-  }
-}
-
 function createServer(port) {
   let state = 10;
 
-  const server = http.createServer(function(request, response) {
+  const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-    console.log(request.url);
-
-    showState(state, request, response);
+    console.log(request.method, request.url);
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/add':
+        state++;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/subtract':
+        state--;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/reset':
+        state = 10;
+        response.write(JSON.stringify({ state }));
+        break;
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ error: 'Not Found' }));
+        break;
+    }
     response.end();
   });
 
