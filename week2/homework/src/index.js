4d841f8f341d1dd4d177023f09bc5f904d187c32
Kelemu <50927723+kelemu-2019@users.noreply.github.com>
2019-09-08 12:30:07 +0200

    Add files via upload

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const commands = require('./commands');
+const program = require('commander');
+
+program
+  .command('add <task>')
+  .alias('a')
+  .description('Add a new to Do List.')
+  .action(task => {
+    commands.add(task);
+  });
+
+program.parse(process.argv);
+
+program
+  .command('list')
+  .alias('l')
+  .description('Lists the to Do List.')
+  .action(() => {
+    commands.list();
+  });
+
+program.parse(process.argv);
+
+program
+  .command('remove <task>')
+  .alias('l')
+  .description('Remove the task from the to Do List.')
+  .action(task => {
+    commands.remove(task);
+  });
+
+program.parse(process.argv);
